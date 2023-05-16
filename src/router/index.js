cb8d24bddfb1912988c1523cf31857d4e8e06f58
hafizhahkhairiyah213510285 <hafizhahkhairiyah@student.uir.ac.id>
2023-05-17 02:01:55 +0700

    update title, menambahkan 404

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/Home.vue'
 import Aplikasi from '../views/AplikasiKu.vue'
+import NotFound from '../views/NotFound.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -14,6 +15,11 @@ const router = createRouter({
       path: '/aplikasiku',
       name: 'aplikasi',
       component: Aplikasi
+    },
+    {
+      path: '/:catchAll(.*)',
+      name: 'NotFound',
+      component: NotFound
     },
   ]
 })
---
+++ src/views/NotFound.vue
@@ -0,0 +1,3 @@
+<template>
+    <h1 class="text-center text-3xl text-medium"><span class="text-bold text-red-700">404</span> Page Not Found!</h1>
+</template>
